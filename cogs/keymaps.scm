e8e5a3ac14dcaf89bbcc7ac53d0927d29abdcc53
Erasin <erasinoo@gmail.com>
2023-11-19 11:31:41 +0800

    plugin system #8675

---
+++ cogs/keymaps.scm
@@ -0,0 +1,65 @@
+(require-builtin helix/core/keymaps as helix.keymaps.)
+
+; (require "steel/contracts/contract.scm"
+;          (for-syntax "steel/contracts/contract.scm")
+;          "steel/contracts/types.scm")
+
+(provide *keybinding-map-set!*
+         *keybinding-map-insert*
+         *get-buffer-or-extension-keybindings*
+         *reverse-buffer-map-insert*
+         merge-keybindings
+         helix-current-keymap
+         set-global-buffer-or-extension-keymap
+         add-global-keybinding
+         deep-copy-global-keybindings)
+
+(define helix-current-keymap helix.keymaps.helix-current-keymap)
+
+;;@doc
+;; Set the keybinding map to the specified map
+(define (*keybinding-map-set!* map)
+  (set-box! helix.keymaps.*buffer-or-extension-keybindings* map))
+
+;;@doc
+;; Insert a key value pair representing a binding to the keybinding map
+(define (*keybinding-map-insert* key value)
+  (set-box! helix.keymaps.*buffer-or-extension-keybindings*
+            (hash-insert (unbox helix.keymaps.*buffer-or-extension-keybindings*) key value)))
+
+;;@doc
+;; Returns the buffer or extension keybinding map
+(define (*get-buffer-or-extension-keybindings*)
+  (unbox helix.keymaps.*buffer-or-extension-keybindings*))
+
+;;@doc
+;; Insert a value into the reverse buffer map
+(define (*reverse-buffer-map-insert* key value)
+  (set-box! helix.keymaps.*reverse-buffer-map*
+            (hash-insert (unbox helix.keymaps.*reverse-buffer-map*) key value)))
+
+;; Marshall values in and out of keybindings, referencing the associated values
+;; within steel
+(define (merge-keybindings keymap steel-key-map)
+  ; (->c helix.keymaps.keymap? hash? void?)
+  (helix.keymaps.helix-merge-keybindings
+   keymap
+   (~> steel-key-map (value->jsexpr-string) (helix.keymaps.helix-string->keymap))))
+
+;;@doc
+;; Check that the types on this map check out, otherwise we don't need to consistently do these checks
+(define (set-global-buffer-or-extension-keymap map)
+  ; (->c (hashof string? helix.keymaps.keymap?) void?)
+  (*keybinding-map-set!* map))
+
+;;@doc
+;; Add keybinding to the global default
+(define (add-global-keybinding map)
+  (helix.keymaps.helix-merge-keybindings
+   helix.keymaps.*global-keybinding-map*
+   (~> map (value->jsexpr-string) (helix.keymaps.helix-string->keymap))))
+
+;;@doc
+;; Deep copy the global keymap
+(define (deep-copy-global-keybindings)
+  (helix.keymaps.helix-deep-copy-keymap helix.keymaps.*global-keybinding-map*))
---
+++ cogs/labelled-buffers.scm
@@ -0,0 +1,107 @@
+(require-builtin helix/core/typable as helix.)
+(require-builtin helix/core/static as helix.static.)
+(require-builtin helix/core/editor)
+
+;; Book keeping for keymaps
+(require (only-in "keymaps.scm" *reverse-buffer-map-insert*))
+
+(provide make-new-labelled-buffer!
+         temporarily-switch-focus
+         open-or-switch-focus
+         currently-in-labelled-buffer?
+         open-labelled-buffer
+         maybe-fetch-doc-id
+         fetch-doc-id)
+
+;; Temporary buffer map, key -> doc id
+(define *temporary-buffer-map* (hash))
+;; Last focused - will allow us to swap between the last view we were at
+(define *last-focus* 'uninitialized)
+
+;; Mark the last focused document, so that we can return to it
+(define (mark-last-focused! cx)
+  (let* ([editor (cx-editor! cx)] [focus (editor-focus editor)])
+    (set! *last-focus* focus)
+    focus))
+
+;; TODO: These appear to be the same function
+(define (currently-focused cx)
+  (~> cx (cx-editor!) (editor-focus)))
+
+;; Grab whatever we're currently focused on
+(define (get-current-focus cx)
+  (~> cx (cx-editor!) (editor-focus)))
+
+;; Get the current document id
+(define (get-current-doc-id cx)
+  (let* ([editor (cx-editor! cx)] [focus (editor-focus editor)]) (editor->doc-id editor focus)))
+
+;;@doc
+;; Attempts to find the doc id associated with the given key, returns #false if
+;; the key doesn't exist
+(define (maybe-fetch-doc-id key)
+  (hash-try-get *temporary-buffer-map* key))
+
+;;@doc
+;; Attempts to find the doc id associated with the given key, errors if
+;; the key does not exist
+(define (fetch-doc-id key)
+  (hash-get *temporary-buffer-map* key))
+
+;;@doc
+;; Creates a new labelled buffer that can be access by the key `label`.
+;; Optionally sets the language type if provided
+(define (make-new-labelled-buffer! cx
+                                   #:label label
+                                   #:language-type (language-type void)
+                                   #:side (side 'none))
+
+  ;; Save our last state to return to it afterwards
+  (define last-focused (currently-focused cx))
+  (define last-mode (~> cx (cx-editor!) (editor-mode)))
+
+  ;; Open up the new labelled buffer in a vertical split, set the language accordingly
+  ;; if it has been passed in
+  (helix.vsplit-new cx '() helix.PromptEvent::Validate)
+
+  ;; Label this buffer - it will now show up instead of `[scratch]`
+  (set-scratch-buffer-name! cx (string-append "[" label "]"))
+
+  (when (eq? side 'left)
+    (helix.static.move-window-far-left cx))
+
+  (when (eq? side 'right)
+    (helix.static.move-window-far-right cx))
+
+  (when language-type
+    (helix.set-language cx (list language-type) helix.PromptEvent::Validate))
+
+  ;; Add the document id to our internal mapping.
+  (set! *temporary-buffer-map* (hash-insert *temporary-buffer-map* label (get-current-doc-id cx)))
+
+  (*reverse-buffer-map-insert* (doc-id->usize (get-current-doc-id cx)) label)
+
+  ;; Go back to where we were before
+  (editor-set-focus! (cx-editor! cx) last-focused)
+  (editor-set-mode! (cx-editor! cx) last-mode))
+
+;; Switch the focus for the duration of the thunk, and return to where we were previously
+(define (temporarily-switch-focus cx thunk)
+  (define last-focused (mark-last-focused! cx))
+  (define last-mode (~> cx (cx-editor!) (editor-mode)))
+  (thunk cx)
+  (editor-set-focus! (cx-editor! cx) last-focused)
+  (editor-set-mode! (cx-editor! cx) last-mode))
+
+(define (open-or-switch-focus cx document-id)
+  (define maybe-view-id? (editor-doc-in-view? (cx-editor! cx) document-id))
+  (if maybe-view-id?
+      (editor-set-focus! (cx-editor! cx) maybe-view-id?)
+      (editor-switch! (cx-editor! cx) document-id)))
+
+(define (open-labelled-buffer cx label)
+  (open-or-switch-focus cx (hash-ref *temporary-buffer-map* label)))
+
+(define (currently-in-labelled-buffer? cx label)
+  (define requested-label (hash-try-get *temporary-buffer-map* label))
+  (equal? (doc-id->usize requested-label) (doc-id->usize (get-current-doc-id cx))))
