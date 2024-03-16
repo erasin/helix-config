19972d67f86fcb8065ebd2c80e3eb7954f3e224f
Erasin <erasinoo@gmail.com>
2024-03-16 14:13:00 +0800

    udpate ime-switch 输入法切换

--- runtime/queries/haxe/textobjects.scm
+++ runtime/queries/haxe/textobjects.scm
@@ -1,9 +1,4 @@
-
-
-
 (class_declaration (_) @class.inside) @class_around 
-
 (function_arg (_) @parameter.inside) @parameter.around
-
 (comment) @comment.inside
 (comment)+ @comment.around
--- runtime/queries/typst/injections.scm
+++
@@ -1,7 +0,0 @@
-(raw_blck
-  (blob) @injection.shebang @injection.content)
-
-(raw_blck
-	lang: (ident) @injection.language
-  (blob) @injection.content)
-
