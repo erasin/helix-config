82d0052691822dbe0e7044b327d5f99dcbf5cd39
Erasin <erasinoo@gmail.com>
2024-09-21 21:58:14 +0800

    rtml inject rust

--- runtime/queries/rstml/injections.scm
+++ runtime/queries/rstml/injections.scm
@@ -1,1 +1,8 @@
 
+((rust_expression) @injection.content
+ (#set! injection.language "rust")
+ (#set! injection.include-children))
+
+((closure_expression) @injection.content
+ (#set! injection.language "rust")
+ (#set! injection.include-children))
